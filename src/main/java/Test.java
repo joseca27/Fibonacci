f2dc2c40197dc7b8d64a2342ac323163b3f0f21e
joseca27 <93703983+joseca27@users.noreply.github.com>
2021-11-04 09:47:42 +0100

    Add files via upload

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	
+	
+	    public static void main(String[] args){
+	        Fibonacci num=new Fibonacci(9);
+	        
+	        for(int i=1;i<=9;i++){
+	            System.out.print(num.result(i)+" ");
+	        }
+	    System.out.println("");
+	    }
+
+}
